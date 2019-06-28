328d2e96babb4e94f140e06feadd2b3f5660beb5
Fangyuan Lin <lin00146@algonquinlive.com>
2019-06-28 12:17:43 -0400

    preparing for deployment

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -19,7 +19,7 @@ import axios from "axios";
 export default {
   data() {
     return {
-      todoList: ["Walk the dog", "Go for a ride"]
+      todoList: []
     };
   },
   components: {
@@ -29,17 +29,25 @@ export default {
   methods: {
     appDeleteTodo(index) {
       this.todoList.splice(index, 1);
+      axios.put(
+        "https://fangyuan-vue-and-axios.firebaseio.com/data.json",
+        this.todoList
+      );
     },
     addTodo(todo) {
       this.todoList.push(todo);
       axios
+        // send the information
         .put(
           "https://fangyuan-vue-and-axios.firebaseio.com/data.json",
           this.todoList
         )
+        // whatever com back from the server
         // put not run , then will run,(response => {}) means :function (response){}
         .then(response => {
+          //
           console.log(response);
+          // status response
           console.log('Your data was saved status: " + response.status');
         })
         // catch the error
@@ -47,7 +55,37 @@ export default {
           console.log(error);
         });
     }
+  },
+
+  // week 8
+  // try other hook
+  // beforeCreate() {
+  //   console.log("before app is created");
+  // },
+  created() {
+    axios
+      .get("https://fangyuan-vue-and-axios.firebaseio.com/data.json")
+      // if successful
+      // arrow function, es6
+      // this "response" is a response
+      .then(response => {
+        console.log(response.data);
+        if (response.data) {
+          this.todoList = response.data;
+        }
+      })
+      // if not successful
+      // "response" is a property of subject
+      .catch(error => {
+        console.log("There was an error in getting data:" + error.response);
+      });
   }
+  // beforeMount() {
+  //   console.log("before app is mounted");
+  // },
+  // mounted() {
+  //   console.log("App is mounted");
+  // }
 };
 </script>
 
